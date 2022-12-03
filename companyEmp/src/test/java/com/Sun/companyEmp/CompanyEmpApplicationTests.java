447701392e4625d702a57eb3b71fdc69e210b5fb
Hamzah312 <64075040+Hamzah312@users.noreply.github.com>
2022-12-04 01:47:16 +0200

    first commit

---
+++ companyEmp/src/test/java/com/Sun/companyEmp/CompanyEmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sun.companyEmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyEmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
