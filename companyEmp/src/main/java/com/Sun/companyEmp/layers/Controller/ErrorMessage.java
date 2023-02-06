a90f369f09ddc53d1d8686a4615844016a5a9526
Hamzah312 <64075040+Hamzah312@users.noreply.github.com>
2023-02-06 06:16:20 +0200

    task 4

---
+++ companyEmp/src/main/java/com/Sun/companyEmp/layers/Controller/ErrorMessage.java
@@ -0,0 +1,18 @@
+package com.Sun.companyEmp.layers.Controller;
+
+public class ErrorMessage {
+     String details;
+
+
+    public ErrorMessage(String details){
+        this.details=details;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
